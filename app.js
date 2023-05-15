431ff69f48626f3abc7010df31663ec6a2214aef
MaxDiWay <41537824+maksim8560@users.noreply.github.com>
2023-05-15 13:51:43 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,40 @@
-// Ожидание загрузки документа перед выполнением JavaScript-кода
-document.addEventListener("DOMContentLoaded", () => {
-  // Получаем HTML-элементы, с которыми мы будем работать
-  const startBtn = document.getElementById("start-game-btn");
-  const gameBoard = document.getElementById("game-board");
-  const score1Elem = document.getElementById("score1");
-  const score2Elem = document.getElementById("score2");
+$(function() {
+	// Обработчик отправки формы
+	$('#loginForm').submit(function(event) {
+		event.preventDefault(); // Отменяем стандартное действие
+		const name = $('#nameInput').val().trim();
+		if (name === '') return; // Ничего не делаем, если имя не указано
 
-  let currentPlayer = null;
-  let player1Score = 0;
-  let player2Score = 0;
+		// Добавляем нового игрока в список
+		$('#playerList ul').append(`<li>${name}</li>`);
 
-  // Функция, которая запускает игру
-  function startGame() {
-    // Предотвращаем повторное нажатие кнопки "Start Game"
-    startBtn.disabled = true;
+		// Если уже два игрока, начинаем игру
+		if ($('#playerList li').length === 2) {
+			startGame();
+		}
 
-    // Отображаем игровое поле
-    gameBoard.style
+		// Очищаем поле ввода имени
+		$('#nameInput').val('');
+	});
+
+	function startGame() {
+		// Создаем игровое поле
+		for (let i = 0; i < 3; i++) {
+			for (let j = 0; j < 3; j++) {
+				$('#gameBoard').append(`<div class="cell" data-row="${i}" data-col="${j}"></div>`);
+			}
+		}
+
+		// Обработчик клика по ячейке
+		$('.cell').click(function() {
+			const row = $(this).data('row');
+			const col = $(this).data('col');
+
+			// Если ячейка уже занята, то ничего не делаем
+			if (gameState[row][col] !== null) return;
+
+			// Определяем, чей ход и меняем состояние поля
+			const playerIndex = $('.currentPlayer').index();
+			gameState[row][col] = playerIndex;
+
+			// Заполняем ячейку и проверяем наличие побед
