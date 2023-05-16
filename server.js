088e80807266b8563b33592620a830658922ec22
MaxDiWay <41537824+maksim8560@users.noreply.github.com>
2023-05-16 10:24:01 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,6 @@
-const WebSocket = require('ws');
+const menuButton = document.querySelector('.menu-button');
+const menu = document.querySelector('.menu');
 
-const wss = new WebSocket.Server({ port: 3000 });
-
-// Обработчик подключения нового клиента
-wss.on('connection', (ws) => {
-	console.log('Новый клиент подключился');
-
-	// Обработчик получения сообщения от клиента
-	ws.on('message', (message) => {
-		const { username, messageText } = JSON.parse(message);
-		console.log(`Получено сообщение от ${username}: ${messageText}`);
-
-		// Отправка сообщения всем клиентам, кроме отправителя
-		wss.clients.forEach((client) => {
-			if (client !== ws && client.readyState === WebSocket.OPEN) {
-				client.send(JSON.stringify({ username, messageText }));
-			}
-		});
-	});
-
-	// Обработчик закрытия соединения клиента
-	ws.on('close', () => {
-		console.log('Клиент отключился');
-	});
+menuButton.addEventListener('click', () => {
+  menu.classList.toggle('open');
 });
