e6cfd8c9000afaf7e15ca35303f3ab42ae06ea97
MaxDiWay <41537824+maksim8560@users.noreply.github.com>
2023-05-16 10:33:55 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,40 @@
-const menuButton = document.querySelector('.menu-button');
-const menu = document.querySelector('.menu');
+const mainBtn = document.getElementById('main-btn');
+const container = document.querySelector('.container');
 
-menuButton.addEventListener('click', () => {
-  menu.classList.toggle('open');
+mainBtn.addEventListener('click', function() {
+	const btnGroup = document.createElement('div');
+	btnGroup.classList.add('btn-group');
+
+	const yesBtn = document.createElement('button');
+	yesBtn.innerText = 'Я ГЕЙ';
+
+	const noBtn = document.createElement('button');
+	noBtn.innerText = 'Я НЕ ГЕЙ!';
+
+	btnGroup.appendChild(yesBtn);
+	btnGroup.appendChild(noBtn);
+
+	container.appendChild(btnGroup);
+
+	mainBtn.style.display = 'none';
+
+	yesBtn.addEventListener('click', function() {
+		btnGroup.style.display = 'none';
+
+		const resultText = document.createElement('div');
+		resultText.id = 'result-text';
+		resultText.innerText = 'Я ЗНАЛ!!!';
+
+		container.appendChild(resultText);
+	});
+
+	noBtn.addEventListener('click', function() {
+		btnGroup.style.display = 'none';
+
+		const resultText = document.createElement('div');
+		resultText.id = 'result-text';
+		resultText.innerText = 'ТЫ ГЕЙ :) ХЫ-ХЫ-ХЫ';
+
+		container.appendChild(resultText);
+	});
 });
