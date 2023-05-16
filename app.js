8d2ba54162b9e1eb0d19bd95b0895d9e8e8109de
MaxDiWay <41537824+maksim8560@users.noreply.github.com>
2023-05-16 09:37:04 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,70 @@
-const imageContainer = document.querySelector('.image-container');
-const overlay = document.querySelector('.overlay');
+const nameModal = document.getElementById("name-modal");
+const nameInput = document.getElementById("name-input");
+const enterButton = document.getElementById("enter-button");
+const messageInput = document.getElementById("message-input");
+const sendButton = document.getElementById("send-button");
+const emojiContainer = document.querySelector(".emoji-container");
 
-imageContainer.addEventListener('mouseenter', () => {
-  overlay.style.display = 'none';
-});
+let username;
 
-imageContainer.addEventListener('mouseleave', () => {
-  overlay.style.display = 'block';
-});
+// Show name modal on page load
+window.onload = function() {
+  nameModal.style.display = "flex";
+};
+
+// Enter button handler
+enterButton.onclick = function() {
+  if (nameInput.value === "") {
+    alert("Name cannot be empty!");
+    return;
+  }
+  username = nameInput.value;
+  nameModal.style.display = "none";
+};
+
+// Send button handler
+sendButton.onclick = function() {
+  if (messageInput.value === "") {
+alert("Message cannot be empty!");
+return;
+}
+
+// Create message element and add to message container
+const messageContainer = document.querySelector(".message-container");
+const messageElement = createMessageElement(username, messageInput.value);
+messageContainer.appendChild(messageElement);
+
+// Clear message input
+messageInput.value = "";
+};
+
+// Emoji container click handler
+emojiContainer.onclick = function(event) {
+const emojiElements = emojiContainer.querySelectorAll(".emoji");
+for (let i = 0; i < emojiElements.length; i++) {
+emojiElements[i].classList.remove("active-emoji");
+}
+if (event.target.classList.contains("emoji")) {
+event.target.classList.add("active-emoji");
+messageInput.value += event.target.innerHTML;
+}
+};
+
+// Helper function to create new message element
+function createMessageElement(name, message) {
+const messageElement = document.createElement("div");
+messageElement.classList.add("message");
+
+const nameElement = document.createElement("span");
+nameElement.classList.add("message-name");
+nameElement.innerHTML = name + ":";
+
+const textElement = document.createElement("span");
+textElement.classList.add("message-text");
+textElement.innerHTML = message;
+
+messageElement.appendChild(nameElement);
+messageElement.appendChild(textElement);
+
+return messageElement;
+}
