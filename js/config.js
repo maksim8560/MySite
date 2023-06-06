fd4c1805ba45b85b4e51372705bc1dadebff6241
MaxDiWay <41537824+maksim8560@users.noreply.github.com>
2023-06-06 13:15:41 +0800

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,51 @@
+// ignore
+var Config = {};
+
+/**
+ * What should the text in the center of the screen be?
+ * if empty it will fill in your Server Name
+ */
+Config.title = "MAJESTIC RP";
+
+/**
+ * Enable map text in the top left corner of the screen?
+ */
+Config.enableMap = true;
+
+/**
+ * Enable steamId text in the top right corner of the screen?
+ */
+Config.enableSteamID = true;
+
+/**
+ * Enable announcements?
+ */
+Config.enableAnnouncements = true;
+
+/**
+ * What messages do you want to show up?
+ * only works if enableAnnouncements = true
+ */
+Config.announceMessages = [
+  "Приятной вам игры!",
+  "Баним читеров, и плохих игроков..",
+  "Говорят что бандиты захвотили город..",
+  "Кажеться в городе видели черепашек ниндзя..."
+];
+
+/**
+ * How many miliseconds for each announcement?
+ * only works if enableAnnouncements = true
+ */
+Config.announcementLength = 5000;
+
+/**
+ * Image Filename
+ * DROP IMAGE IN "images" FOLDER
+ */
+Config.backgroundImage = "default.jpg";
+
+/**
+ * Enable debug messages?
+ */
+Config.enableDebug = false;
